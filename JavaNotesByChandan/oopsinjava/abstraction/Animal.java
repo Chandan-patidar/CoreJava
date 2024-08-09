a0da453970619ce1cc363b1b70980f6f1ed63787
chandan patidar <chandan15362@gmail.com>
2024-08-10 00:47:52 +0530

    Example of

--- JavaNotesByChandan/oopsinjava/abstraction/Animal.java
+++ JavaNotesByChandan/oopsinjava/abstraction/Animal.java
@@ -1,5 +1,26 @@
 package JavaNotesByChandan.oopsinjava.abstraction;
 // abstract class
 abstract class Animal {
-    // abstract method (does't not have body)
+    // abstract method (does not have body)
+    public abstract void animalSound();
+    // Regular method
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+}
+
+// subclass(inherit from Animal)
+class Pig extends Animal {
+    public void animalSound() {
+        //The body of animalSound() is provided here
+        System.out.println("This Pig say : wee wee");
+    }
+}
+class main{
+    public static void main(String[] args){
+        Pig myPig = new Pig(); // create a pig object
+        myPig.animalSound();
+        myPig.sleep();
+
+    }
 }
